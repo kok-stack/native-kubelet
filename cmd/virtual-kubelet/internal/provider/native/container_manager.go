81a1879b0a4fbf5ee85c388cd423c280051c2680
苏连云 <562050688@qq.com>
2021-03-18 18:08:34 +0800

    实现 创建process

--- cmd/virtual-kubelet/internal/provider/native/container_manager.go
+++ cmd/virtual-kubelet/internal/provider/native/container_manager.go
@@ -2,18 +2,34 @@ package native
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/flytam/filenamify"
+	"github.com/prologic/bitcask"
+	"github.com/shirou/gopsutil/process"
 	corev1 "k8s.io/api/core/v1"
+	"os"
+	"os/exec"
 	"path/filepath"
 )
 
 type ContainerManager struct {
-	workdir string
+	workdir   string
+	processDb *bitcask.Bitcask
 }
 
-func NewContainerManager(workdir string) *ContainerManager {
-	return &ContainerManager{workdir: workdir}
+func NewContainerManager(workdir string, processDb *bitcask.Bitcask) *ContainerManager {
+	return &ContainerManager{workdir: workdir, processDb: processDb}
+}
+
+type ContainerProcess struct {
+	workdir   string
+	podName   string
+	namespace string
+}
+
+func (p *ContainerProcess) Run(ctx context.Context) error {
+	//command := exec.Command()
 }
 
 /*
@@ -33,8 +49,35 @@ func (m *ContainerManager) createContainer(ctx context.Context, im *ImageManager
 		return err
 	}
 	//配置运行process
-
+	pro, err := m.createContainerProcess(pod, containerWorkDir(workdir))
 	//运行
+	return pro.Run(ctx)
+}
+
+func (m *ContainerManager) createContainerProcess(pod *corev1.Pod, workdir string) (*ContainerProcess, error) {
+	process := NewProcess(pod, workdir)
+	marshal, err := json.Marshal(process)
+	if err != nil {
+		return nil, err
+	}
+	err = m.processDb.Put(getProcessKey(process), marshal)
+	if err != nil {
+		return nil, err
+	}
+	return process, nil
+}
+
+func NewProcess(pod *corev1.Pod, workdir string) *ContainerProcess {
+	//TODO:运行命令，环境变量等参数设置
+	return &ContainerProcess{
+		workdir:   workdir,
+		podName:   pod.Name,
+		namespace: pod.Namespace,
+	}
+}
+
+func getProcessKey(process *ContainerProcess) []byte {
+	return []byte(fmt.Sprintf("process:%s:%s", process.namespace, process.podName))
 }
 
 func getContainerWorkDir(workdir string, pod *corev1.Pod, c corev1.Container) (string, error) {
